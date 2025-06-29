663f75024492cf554180e5b0b3bd52e481073a64
trindadedev13 <trindadedev13@gmail.com>
2025-06-29 09:29:27 -0300

    feat(all): Add comments

--- include/io.h
+++ include/io.h
@@ -18,13 +18,20 @@ extern file std_files[];
 extern long __ASM_WRITE__(int fd, const buffer buffer, size_t len);
 extern long __ASM_EXIT__(long status);
 
+// Writes a content(buffer) to a file
 void write(const file *f, const buffer buf, size_t len);
+
+// Finishes the program
 void exit(long exit_code);
 
+// Writes a char in stdout.
 void put_char(char c);
+// Writes a string in stdout.
 void put_str(string s);
 
+// Writes a int in stdout.
 void print_int(int n);
+// Writes a formatted string in stdout.
 void printf(const string fmt, ...);
 
 #endif
--- include/string.h
+++ include/string.h
@@ -7,14 +7,26 @@
 
 size_t str_len(const string str);
 
+// Compares the strings
+// Returns positive if a > b
+// Returns negative if a < b
+// Returns 0 if a == b
 int str_cmp(string a, string b);
+// Compares the bytes up to 'lmt' of a & b
+// Returns positive if a > b
+// Returns negative if a < b
+// Returns 0 if a == b
 int str_cmp_lmt(string a, string b, size_t lmt);
 
+// Compare the string and check if they are equals
 bool str_equals(string a, string b);
 
+// Concatenates a string new in string src
 string str_cat(string src, string new);
 
+// Copies all string
 string str_copy(string dest, string src);
+// Copies just 'lmt' bytes of string src to dest
 string str_copy_lmt(string dest, string src, size_t lmt);
 
 #endif
